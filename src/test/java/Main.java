aad04276e2ac01fd4a3d8410d5f4675bf5cb90bd
Marsik <timofeyzalesov@gmail.com>
2021-06-23 14:09:28 +0300

    123

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,6 +1,8 @@
 import models.Auto;
 import models.Person;
 import services.UserService;
+import utils.HibernateSessionFactoryUtil;
+
 import java.sql.SQLException;
 
 public class Main {
@@ -8,16 +10,24 @@ public class Main {
 
 
         UserService userService = new UserService();
-        Person person = new Person("Masha",26);
-        userService.saveUser(person);
-        Auto ferrari = new Auto("Ferrari", "red");
-        person.addAuto(ferrari);
-        Auto ford = new Auto("Ford", "black");
-        ford.setPerson(person);
-        person.addAuto(ford);
-        userService.updateUser(person);
-        person.setName("Roma");
-        userService.updateUser(person);
+//        Person person = new Person("Masha",26);
+//        Auto ferrari = new Auto("Ferrari", "red");
+//        person.addAuto(ferrari);
+//        ferrari.setPerson(person);
+//        userService.saveUser(person);
+
+//        Auto ford = new Auto("Ford", "black");
+//        ford.setPerson(person);
+//        person.addAuto(ford);
+//        userService.updateUser(person);
+//        person.setName("Vasya");
+//        person.setAge(34);
+//        userService.updateUser(person);
+//       toChange.removeAuto(userService.findAuto(1));
+//       toChange.removeAuto(userService.findAuto(2));
+        System.out.println(userService.findUser(7));
+
+
 
 
     }
